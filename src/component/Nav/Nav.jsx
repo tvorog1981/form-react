608ab9f3d169480a4a6b8a13cb2f50b52fe10a82
DSmetankin <DSmetankin@tran.local>
2025-11-11 17:19:25 +0300

    nav

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,26 +1,26 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 export default function Nav() {
   return (
     <nav className=" flex gap-1 bg-amber-300 justify-center items-center mb-2 container mx-auto">
-      <Link
+      <NavLink
         className="uppercase font-bold text-white p-2 hover:text-teal-500"
         to={"/"}
       >
         Main
-      </Link>
-      <Link
+      </NavLink>
+      <NavLink
         className="uppercase font-bold text-white p-2 hover:text-teal-500"
         to={"/form"}
       >
         FORM
-      </Link>
-      <Link
+      </NavLink>
+      <NavLink
         className="uppercase font-bold text-white p-2 hover:text-teal-500"
         to={"/form-yup"}
       >
         FORM YUP
-      </Link>
+      </NavLink>
     </nav>
   );
 }
